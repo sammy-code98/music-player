b61fd3efca179de966615f7d399c725c7d2cd42c
uzor samuel ugochukwu <sammysmart98@yahoo.com>
2021-08-19 02:36:21 +0100

    update

--- scritp.js
+++ scritp.js
@@ -5,7 +5,24 @@ const playBtn = document.querySelector("#play");
 const nextBtn = document.querySelector("#next");
 
 const audio = document.querySelector("#audio");
-const progress = document.querySelector(".progress")
-const progressContainer = document.querySelector(".progress-container")
-const title = document.querySelector(".title")
-const coverImg = document.querySelector("#cover-image")
+const progress = document.querySelector(".progress");
+const progressContainer = document.querySelector(".progress-container");
+const title = document.querySelector(".title");
+const coverImg = document.querySelector(".img-animate");
+
+// titles of songs
+
+const songs = ["Empty", "Glorious", "Many Battles", "Access Bank"];
+
+// track songs
+
+let songIndex = 3;
+// load song into DOM
+
+loadSong(songs[songIndex]);
+
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `songs/${song}.mp3`;
+  coverImg.src = `images/${song}.jpeg`
+}
