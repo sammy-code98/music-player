9f00f7e1392dc1b6bdb007c6ab76b0b3fedd52d6
uzor samuel ugochukwu <sammysmart98@yahoo.com>
2021-08-19 02:08:44 +0100

    add song cover image and js file

---
+++ scritp.js
@@ -0,0 +1,11 @@
+const containerMusic = document.querySelector(".container");
+
+const prevBtn = document.querySelector("#prev");
+const playBtn = document.querySelector("#play");
+const nextBtn = document.querySelector("#next");
+
+const audio = document.querySelector("#audio");
+const progress = document.querySelector(".progress")
+const progressContainer = document.querySelector(".progress-container")
+const title = document.querySelector(".title")
+const coverImg = document.querySelector("#cover-image")
